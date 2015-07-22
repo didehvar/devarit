0ba0a9e39ed54e3993f66276dc6afa7070f09c3e
james <james@didehvar.com>
2015-07-22 22:23:57 +0100

    Livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
+var livereload = require('gulp-livereload');
 
-gulp.task('default', function() {
+gulp.task('less', function() {
   return gulp.src('./less/**/*.less')
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
     .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch('less/**/*.less', ['less']);
 });
