8de463d7f62fd959362cfdc730838995212288e1
james <james@didehvar.com>
2015-07-23 22:42:57 +0100

    Auto updating navbar wizardry

--- js/scroll-navbar.js
+++ js/scroll-navbar.js
@@ -4,6 +4,38 @@ $(document).ready(function() {
     $('.navbar').addClass('navbar-fixed-top');
   }
 
+  function checkNav() {
+    // -50 accounts for offset in .navbar-anchor-fixed
+    // hey maybe we can jquery that
+    var offset = parseInt($('.navbar-anchor-fixed').css('top'), 10); // we can so jquery that!
+
+    if ($(this).scrollTop() >= $('#section-clients').offset().top + offset) {
+      if ($(this).scrollTop() >= $('#section-contact').offset().top + offset) {
+        return setNav('contact');
+      } else {
+        return setNav('clients');
+      }
+    }
+
+    return setNav('home');
+  }
+
+  function setNav(ele) {
+    if (ele == currentNav) {
+      return;
+    }
+
+    currentNav = ele;
+
+    $('.navbar-nav > li').each(function() {
+      $(this).removeClass('active');
+    });
+
+    $('.navbar-nav a[href="#' + ele + '"]').parent().addClass('active');
+  }
+
+  var currentNav = 'home';
+
   $(document).scroll(function() { // navbar fixed to top scrolling
     if ($(this).scrollTop() > $('#nav').offset().top) {
       // scrolled below nav, fix it
@@ -15,6 +47,8 @@ $(document).ready(function() {
       // special cases for home anchor
       $('#home').addClass('navbar-anchor-none');
       $('#home').removeClass('navbar-anchor-home');
+
+      checkNav();
     } else {
       // scrolled above nav, unfix it
       $('.navbar').removeClass('navbar-fixed-top');
@@ -31,16 +65,12 @@ $(document).ready(function() {
   $('.navbar-nav a').click(function(event) { // pretty scrolling
     event.preventDefault();
 
-    var link = this;
+    var link = this.hash;
     $.smoothScroll({
-      scrollTarget: link.hash
+      scrollTarget: link
     });
 
-    $('.navbar-nav > li').each(function() {
-      $(this).removeClass('active');
-    });
-
-    $(event.target).parent().addClass('active');
+    // setNav(link.substr(1, link.length));
   });
 
   $('.home-helper a').click(function(event) { // scroll down from main page
