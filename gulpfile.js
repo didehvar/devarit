336cdcf05b78baa228a8a9f921fb57831a3395df
james <james@didehvar.com>
2015-07-23 15:03:22 +0100

    Remove some dependencies, fix some cdn things

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,33 @@ var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 
 gulp.task('less', function() {
-  return gulp.src('./less/devarit.less')
+  return gulp.src('less/devarit.less')
     .pipe(plugins.less())
     .pipe(gulp.dest('css'))
     .pipe(plugins.livereload());
 });
 
-gulp.task('html', function() {
-  return gulp.src('index.html')
-    .pipe(plugins.livereload());
-})
+gulp.task('build', function() {
+  gulp.src(['node_modules/bootstrap/dist/css/bootstrap.min.css',
+            'node_modules/font-awesome/css/font-awesome.min.css'])
+      .pipe(gulp.dest('css'));
+
+  gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js',
+            'node_modules/jquery/dist/jquery.min.js',
+            'node_modules/jquery-smooth-scroll/src/jquery.smooth-scroll.js'])
+      .pipe(gulp.dest('js/vendor'));
+
+  gulp.src('node_modules/font-awesome/fonts/*.*')
+      .pipe(gulp.dest('fonts'));
+
+  gulp.src('index.html')
+      .pipe(plugins.livereload());
+});
 
 gulp.task('watch', function() {
+  gulp.start('build');
+
   plugins.livereload.listen();
   gulp.watch('less/*.less', ['less']);
-  gulp.watch('index.html', ['html']);
+  gulp.watch('index.html', ['build']);
 });
--- js/scroll-navbar.js
+++ js/scroll-navbar.js
@@ -1,28 +1,25 @@
 $(document).ready(function() {
   $(document).scroll(function() {
-    if ($(this).scrollTop() > $('#nav-start').offset().top) {
+    if ($(this).scrollTop() > $('#nav').offset().top) {
+      // scrolled below nav, fix it
       $('.navbar').addClass('navbar-fixed-top');
+
+      // fix anchors (account for fixed navbar offset)
+      $('.navbar-anchor').addClass('navbar-anchor-fixed');
+
+      // special cases for home anchor
+      $('#home').addClass('navbar-anchor-none');
+      $('#home').removeClass('navbar-anchor-home');
     } else {
+      // scrolled above nav, unfix it
       $('.navbar').removeClass('navbar-fixed-top');
+
+      // fix anchors (account for fixed navbar offset)
+      $('.navbar-anchor').removeClass('navbar-anchor-fixed');
+
+      // special cases for home anchor
+      $('#home').addClass('navbar-anchor-home');
+      $('#home').removeClass('navbar-anchor-none');
     }
   })
 });
-
-
-// $(document).ready(function() {
-//   /* http://stackoverflow.com/questions/23706003/changing-nav-bar-color-after-scrolling */
-//   var scroll_start = 0;
-//   var start_change = $('#change-navbar');
-//   var offset = start_change.offset();
-//
-//   if (start_change.length){
-//     $(document).scroll(function() {
-//       scroll_start = $(this).scrollTop();
-//       if(scroll_start > offset.top) {
-//         $('.header .navbar-default').css('background-color', 'rgba(0, 0, 0, 0.5)');
-//       } else {
-//         $('.header .navbar-default').css('background-color', 'transparent');
-//       }
-//     });
-//   }
-// });
